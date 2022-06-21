44b02ac5b16f3d9828f0cd948934ec1dffc11ef2
이용빈 <54783158+yblee1223@users.noreply.github.com>
2022-06-22 04:59:38 +0900

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,10 +1,25 @@
+#include <string.h>
+// LED
 #include <Adafruit_NeoPixel.h> // led
 // RFID
-// CDS
+#include <SPI.h>
+#include <MFRC522.h>
+#define RST_PIN         9           // Configurable, see typical pin layout above
+#define SS_PIN          10          // Configurable, see typical pin layout above
+// SONIC
+#define PIN 7
+#define PIN1 8
+#define NUMPIXELS 27 //소자 개수 파악
+#define BRIGHTNESS 100
 // LCD
+#include<LiquidCrystal.h> //LCD 라이브러리
+
+char car_num[100];
+int D; // disable
+int W;// wheel chair
 // ACT
 // SONIC
-// LED
+
 #define MAXPIN 9 
 #define NUMPIXELS 27
   
@@ -14,7 +29,6 @@ typedef struct sector { // parking sector
     int sector_num;
 
     // CDS
-    int cdsValue;
     int car_in;
     
     // LED
@@ -41,18 +55,28 @@ Adafruit_NeoPixel selector = Adafruit_NeoPixel(NUMPIXELS, pin_num[0], NEO_GRB + 
 // CDS
 int cds[8] = {A0, A1, A2, A3, A4, A5, A6, A7}; // analogpin
 // RFID
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 // LCD
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
 // ACT
 // SONIC
+int trigPin = 2;
+int echoPin = 3;
 
 
 /* --- init func --- */
-void init_led();
 void init_sector();
+void init_led();
+void init_rfid();
+void init_sonic();
+
+
+
+// main
 void led_main();
+
+//led
 void select(int n);
-
-
 // bright
 void bright_H();
 void bright_playground();
@@ -62,14 +86,14 @@ void bright_allsector();
 void bright_sector(int sector_num, char color);
 void bright_7_segment();// 가운데 걸로 7segment 주차 대수 보여주기
 
-// cds
-void is_carin(); // 섹터에 차량 있는지 확인
 
 void setup() 
 {
   Serial.begin(9600);
-  init_led();
   init_sector();
+  init_led();
+  init_rfid();
+  lcd.begin(16, 2); // LCD
   //bright_H();
   //bright_playground();
 }
@@ -87,20 +111,7 @@ void loop()
 /* ---- func ---- */
 
 // init
-void init_led()
-{
-  // pinMode
 
-  for (int i = 0; i < 9; i++){
-    pinMode(pin_num[i], OUTPUT);
-  }
-  // This initializes the NeoPixel library.
-  
-  pinMode(sw[0], INPUT);
-  pinMode(sw[1], INPUT);
-  pinMode(sw[2], INPUT);
-  bright_off();
-}
 
 void init_sector()
 {
@@ -139,8 +150,36 @@ void init_sector()
         s[i].j_hat[1] = led_line_j_hat[i % 4 + 1];
     }
 }
+void init_rfid()
+{
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
+}
 
+void init_led()
+{
+  // pinMode
 
+  for (int i = 0; i < 9; i++){
+    pinMode(pin_num[i], OUTPUT);
+  }
+  // This initializes the NeoPixel library.
+  
+  pinMode(sw[0], INPUT);
+  pinMode(sw[1], INPUT);
+  pinMode(sw[2], INPUT);
+  bright_off();
+}
+
+void init_sonic()
+{
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
 
 
 void dps_main()
@@ -153,20 +192,223 @@ void dps_main()
   // GATE
   // CDS
   // LED
+  rfid_main();
   cds_main();
   led_main();
 }
+// DPS
+// LCD
+void lcd_main()
+{
+  if 
+  lcd.setCursor(5, 0);  //커서를 (5, 0)으로 보내라
+  lcd.print("마이더부"); //(5, 0)부터 Hello!!를 찍어라
+  lcd.setCursor(1,1);   //커서를 (1,1)로 옮겨라
+  lcd.print("선형대수"); //(1,1)부터 Codingrun.com을 찍어라
+}
+// SONIC
+//*****************************************************************************************//
+void sonic_main()
+{
+  digitalWrite(trigPin, LOW);
+  delay(30);
+  digitalWrite(trigPin, HIGH);
+  delay(30);
+  digitalWrite(trigPin, LOW); //초음파 보내고 받기
+  float duration = pulseIn(echoPin, HIGH); //마이크로 초 구하기
+  float distance = ((float)            00) / 2; //거리 구하기
 
+  if (distance < 7):
+    Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, pin_num[4], NEO_GRB + NEO_KHZ800);
+    Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(NUMPIXELS, pin_num[5], NEO_GRB + NEO_KHZ800);
+    hps(distance);
+}
+
+void hps(int distance)// 주차 보조 시스템
+{ 
+  while(distance > 2){
+  if(distance >= 2 && distance <= 3){
+    colorWipe1(strip.Color(0,0,100), strip1.Color(0,0,100), 0);
+    Serial.print(distance);
+    Serial.println("r\n");
+    exit(1);
+  }
+  else if(distance > 4){
+    colorWipe1(strip.Color(100,100,0),strip1.Color(100,100,0),50);
+    Serial.print(distance);
+    Serial.println("l\n");
+  }
+  else if(distance < 2){
+    colorReWipe(strip.Color(100,0,0),strip1.Color(100,0,0),50);
+    Serial.print(distance);
+    Serial.println("s\n");
+  }
+  delay(3000);
+  }
+}
+void colorWipe1(uint32_t c, uint32_t c1, uint8_t wait){
+  for(int i = 0; i < NUMPIXELS;i+=2){
+    strip.setPixelColor(i,c);
+    stripe.setPixelColor(i,c1);
+    strip.show();
+    stripe.show();
+    delay(wait);
+  }
+}
+void colorReWipe(uint32_t c, uint32_t c1, uint8_t wait){
+  for(int i = NUMPIXELS - 1; i>=0; i-=2){
+    strip.setPixelColor(i,c);
+    stripe.setPixelColor(i,c1);
+    strip.show();
+    stripe.show();
+    delay(wait);
+  }
+}
+// RFID
+//*****************************************************************************************//
+void rfid_main()
+{
+  read_data();
+}
+void read_data()
+{
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  //some variables we need
+  byte block;
+  byte len;
+  MFRC522::StatusCode status;
+
+  //-------------------------------------------
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.println(F("**Card Detected:**"));
+
+  //-------------------------------------------
+
+  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
+
+  //-------------------------------------------
+  int cnt = 0;
+  
+
+  byte buffer1[18];
+  char str1[100] = " "; // 2진수 입력
+  int a = 0, b = 0;
+  block = 1;
+  len = 18;
+
+  //------------------------------------------- GET binary code
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer1, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  Serial.print(F("data 1, data 2: "));
+  //PRINT FIRST NAME
+  for (uint8_t i = 0; i < 16; i++)
+  {
+    if (buffer1[i] != 32)
+    {
+      Serial.write(buffer1[i]);
+      if (buffer1[i] == ' ')
+        break;
+      str1[i] = buffer1[i];
+    }
+  }
+  Serial.println();
+  a = str1[0]-'0';
+  b = str1[1]-'0';
+  D = a;
+  W = b;
+  Serial.print("장애인 차량: ");
+  Serial.println(a);
+  Serial.print("휠체어 유무: ");
+  Serial.println(b);
+  //Serial.println(str1);
+  //Serial.println(strlen(str1));
+  
+  Serial.println(" ");
+  
+  //---------------------------------------- GET car number
+
+  byte buffer2[18];
+  char str2[100] = " "; // 차량번호
+  block = 4;
+
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid)); //line 834
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer2, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  cnt = 0;
+  Serial.print(F("data 3: "));
+  //PRINT LAST NAME
+  for (uint8_t i = 1; i < 16; i++) {
+    Serial.write(buffer2[i] );
+    if (buffer2[i] == ' '){
+      if (cnt == 1)
+        break;
+      cnt = 1;
+    }
+    str2[i-1] = buffer2[i];
+  }
+  strcpy(car_num, str2);
+  //Serial.println();
+  //Serial.print("차량번호");
+  //Serial.println(str2);
+  //Serial.println(strlen(str2));
+
+
+  //----------------------------------------
+
+  Serial.println(F("\n**End Reading**\n"));
+
+  delay(1000); //change value if you want to read cards faster
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+}
 
 
 // CDS
+//*****************************************************************************************//
 void cds_main()
 {
   cds_state();
   //find_parking_place();
   pps();
 }
-void cds_state();
+void cds_state()
 {
   for (int i = 0; i< 8; i++){
     cdsValue = analogRead(cds[i]);
@@ -218,6 +460,7 @@ void pps()//장애인 구역 보존 알고리즘
 
 
 // LED
+//*****************************************************************************************//
 void select(int n)
 {
   selector = Adafruit_NeoPixel(NUMPIXELS, pin_num[n], NEO_GRB + NEO_KHZ800);
