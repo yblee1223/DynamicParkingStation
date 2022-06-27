9bd4ce7a328052d1800eff1520691405de46272d
이용빈 <54783158+yblee1223@users.noreply.github.com>
2022-06-27 22:06:32 +0900

    Update display_carnumber.ino

--- src/RFID/display_carnumber.ino
+++ src/RFID/display_carnumber.ino
@@ -25,7 +25,7 @@
 #include <MFRC522.h>
 #include<LiquidCrystal.h> //LCD 라이브러리를 추가
 //LiquidCrystal lcd(RS, Enable, DB4, DB5, DB6, DB7);
-LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 
 #define RST_PIN         9           // Configurable, see typical pin layout above
@@ -166,13 +166,11 @@ void loop() {
 
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
+  
+  lcd.setCursor(1, 0);  //커서를 (5, 0)으로 보내라
+  lcd.print("CarNum:");
+  lcd.print(str2);
 
-
-  lcd.setCursor(5, 0);  //커서를 (5, 0)으로 보내라
-  lcd.print(str2); //(5, 0)부터 Hello!!를 찍어라
-  if (a == 1){
-    lcd.setCursor(1,1);   //커서를 (1,1)로 옮겨라
-    lcd.print("Go blue Light"); //(1,1)부터 Codingrun.com을 찍어라.
-  }
+  
 }
 //*****************************************************************************************//
